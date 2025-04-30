7dd8440b122c89e462a4a8ea786880d0c37880fd
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-04-30 11:17:31 +0700

    feat: update env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -12,6 +12,21 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+// @title           GoChat Backend API
+// @version         1.0
+// @description     A Real-time Chat Application Backend.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
 var listEnvSecret = []string{
 	"Constants",
 	"MysqlPassword",
