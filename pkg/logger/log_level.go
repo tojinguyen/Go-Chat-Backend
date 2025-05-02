6d00e754712aa15d07d3af6d22210990d62ca051
Nguyễn Văn Toại <122630929+tojinguyen@users.noreply.github.com>
2025-05-02 22:03:11 +0700

    Feat/log (#1)
    * feat: add system error
    
    * feat: add logger system
    
    * update: update logger condition

---
+++ pkg/logger/log_level.go
@@ -0,0 +1,11 @@
+package logger
+
+type LogLevel string
+
+var (
+	LogLevelError LogLevel = "ERROR"
+	LogLevelInfo  LogLevel = "INFO"
+	LogLevelDebug LogLevel = "DEBUG"
+	LogLevelWarn  LogLevel = "WARN"
+	LogLevelFatal LogLevel = "FATAL"
+)
