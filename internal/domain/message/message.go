fe0610ea5c0ee6896b5d49bb0e395239bfb0b806
tojinguyen <toainguyenprogramminggame@gmail.com>
2025-05-13 22:06:02 +0700

    feat: add message type, struct

---
+++ internal/domain/message/message.go
@@ -0,0 +1,28 @@
+package domain
+
+type MessageType string
+
+const (
+	TextMessageType  MessageType = "TEXT"
+	ImageMessageType MessageType = "IMAGE"
+	VideoMessageType MessageType = "VIDEO"
+	AudioMessageType MessageType = "AUDIO"
+	FileMessageType  MessageType = "FILE"
+)
+
+type Message struct {
+	ID         string      `json:"id"`
+	SenderId   string      `json:"sender_id"`
+	ReceiverId string      `json:"receiver_id"`
+	Type       MessageType `json:"type"`
+	Content    string      `json:"content"`
+	CreatedAt  string      `json:"created_at"`
+}
+
+type ChatRoom struct {
+	ID          string   `json:"id"`
+	Name        string   `json:"name"`
+	Type        string   `json:"type"` // "GROUP" or "PRIVATE"
+	CreatedAt   string   `json:"created_at"`
+	LastMessage *Message `json:"last_message"`
+}
